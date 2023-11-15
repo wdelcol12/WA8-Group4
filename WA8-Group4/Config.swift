45ae8e92be7610eb37abf02cac433520010d8f29
Rishabh Raj <raj.rishabh01@gmail.com>
2023-11-15 05:47:09 +0530

    re-checkin

---
+++ WA8-Group4/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  WA8-Group4
+//
+//  Created by Rishabh Raj on 13/11/23.
+//
+
+
+import Foundation
+class Configs{
+    static let tableViewMessagesID = "tableViewMessagesID"
+}
+
--- WA8-Group4/Messenger/MessengerViewController.swift
+++ WA8-Group4/Messenger/MessengerViewController.swift
@@ -13,7 +13,8 @@ class MessengerViewController: UIViewController {
 
     let msgView = MessengerView()
     let database = Firestore.firestore()
-    
+//    var friends: [Friend] = [] // Assuming friends is an array of strings
+    var friends: [String] = [] // Assuming friends is an array of strings
     override func loadView() {
         view = msgView
     }
@@ -25,7 +26,49 @@ class MessengerViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logout))
         navigationItem.hidesBackButton = true
         
+        msgView.tableViewMessages.delegate = self
+        msgView.tableViewMessages.dataSource = self
+        
+        //MARK: removing the separator line...
+        msgView.tableViewMessages.separatorStyle = .none
+        
+        
         loadFriends()
+    }
+    
+    @objc func loadFriends() {
+        let userCollections = database.collection("users")
+
+        userCollections.getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                for document in querySnapshot!.documents {
+                    let data = document.data()
+                    if let name = data["name"] as? String {
+                        print("Hahaha: ", data["email"], UserDefaults.standard.string(forKey: "userToken"))
+                        if(data["email"] as? String != UserDefaults.standard.string(forKey: "userToken")) {
+//                            let friend  = try document.data(as: Friend.self)
+//                            let friend = Friend(name: data["name"], lastMessage: data["name"], time: data["name"])
+                            self.friends.append(name)
+                        }
+                        
+                    }
+                }
+
+                // Now you can use the 'friends' array as needed
+                print("Friends: \(self.friends)")
+//                self.msgView.email.text = friends.joined()
+                self.msgView.tableViewMessages.reloadData()
+            }
+        }
+    }
+
+    //Change from String to Chat Model
+    func openChatWindow(name: String){
+        let chatViewController = ChatViewController()
+        chatViewController.chatSelected = name
+        navigationController?.pushViewController(chatViewController, animated: true)
     }
     
     @objc func loadFriends() {
