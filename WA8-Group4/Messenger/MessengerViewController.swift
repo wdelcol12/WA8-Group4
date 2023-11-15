d7af9f50147958f2d7e4737f34f6cbe20638a2e4
Rishabh Raj <raj.rishabh01@gmail.com>
2023-11-15 05:52:19 +0530

    edit to message controller

--- WA8-Group4/Messenger/MessengerViewController.swift
+++ WA8-Group4/Messenger/MessengerViewController.swift
@@ -71,33 +71,6 @@ class MessengerViewController: UIViewController {
         navigationController?.pushViewController(chatViewController, animated: true)
     }
     
-    @objc func loadFriends() {
-        let userCollections = database.collection("users")
-        var friends: [String] = [] // Assuming friends is an array of strings
-
-        userCollections.getDocuments { (querySnapshot, error) in
-            if let error = error {
-                print("Error getting documents: \(error)")
-            } else {
-                for document in querySnapshot!.documents {
-                    let data = document.data()
-                    if let name = data["name"] as? String {
-                        print("Hahaha: ", data["email"], UserDefaults.standard.string(forKey: "userToken"))
-                        if(data["email"] as? String != UserDefaults.standard.string(forKey: "userToken")) {
-                            friends.append(name)
-                        }
-                        
-                    }
-                }
-
-                // Now you can use the 'friends' array as needed
-                print("Friends: \(friends)")
-                self.msgView.email.text = friends.joined()
-            }
-        }
-    }
-
-    
     @objc func logout() {
         do {
             try Auth.auth().signOut()
