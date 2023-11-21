2b2f148cb25628c35197d4329fc811ebf5f4ef22
Bhavik-20 <bbhatt2005@gmail.com>
2023-11-20 19:18:19 -0500

    Message screen real-time

--- WA8-Group4/Messenger/MessengerViewController.swift
+++ WA8-Group4/Messenger/MessengerViewController.swift
@@ -36,7 +36,7 @@ class MessengerViewController: UIViewController {
         //MARK: removing the separator line...
         msgView.tableViewMessages.separatorStyle = .none
         
-//        loadFriends()
+        loadFriends()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,17 +47,18 @@ class MessengerViewController: UIViewController {
     
     @objc func loadFriends() {
         let userCollections = database.collection("users")
-        
+        print("see 1")
         userCollections.getDocuments { (querySnapshot, error) in
             if let error = error {
                 print("Error getting documents: \(error)")
             } else {
                 for document in querySnapshot!.documents {
                     let data = document.data()
-                    
+                    print("see 2")
                     if let name = data["name"] as? String, let email = data["email"] as? String {
                         if(data["email"] as? String != UserDefaults.standard.string(forKey: "userToken")) {
                             //find the chatID
+                            print("see 3")
                             self.createFriend(name: name, email: email)
                         }
                         
@@ -68,6 +69,7 @@ class MessengerViewController: UIViewController {
     }
     
     @objc func createFriend(name: String, email: String){
+        print("see 4")
         self.friends.removeAll()
         
         let receipentUser = email
@@ -81,95 +83,58 @@ class MessengerViewController: UIViewController {
             .collection("messages")
             .order(by: "timestamp", descending:false)
             .addSnapshotListener(includeMetadataChanges: false, listener: {querySnapshot, error in
-                
+                print("see 5")
                 if let documents = querySnapshot?.documents{
-                    
-                    
+                    print("see 6")
                     msgArray.removeAll()
-                    
-                    for document in documents{
-                        
-                        
+                    for document in documents {
                         do {
+                            print("see 7")
                             let msg = try document.data(as: Message.self)
                             msgArray.append(msg)
                             
-                        }catch {
+                        } catch {
                             print(error)
                         }
                     }
-                        
-                        var lastMessage: String?
-                        var timestamp: String?
-                        
-                        if let last = msgArray.last {
-                            lastMessage = last.text
-                            timestamp = last.timestamp
-                            // Use the lastMessage here
-                            print("Last Message: \(String(describing: lastMessage))")
-                        } else {
-                            // The array is empty
-                            lastMessage = ""
-                            timestamp = ""
-                        }
-                        
-                        if let lastMessageText = lastMessage, let lastMessageTimestamp = timestamp {
-                            let friend = Friend(name: name, lastMessage: lastMessageText, time: lastMessageTimestamp, email: email)
-                            print("Check: ", lastMessageText)
-                            self.friends.append(friend)
-                            
-                        } else {
-                            // Handle the case where lastMessageText or lastMessageTimestamp is nil
-                            print("No last message available")
-                        }
-                        // Sort the friends array based on timestamp
-                        self.friends.sort { (friend1, friend2) -> Bool in
-                            // Assuming timestamp is a String in the format of a date
-                            return friend1.time > friend2.time
-                        }
-                        self.msgView.tableViewMessages.reloadData()
+                    
+                    var lastMessage: String?
+                    var timestamp: String?
+                    
+                    if let last = msgArray.last {
+                        print("see 8")
+                        lastMessage = last.text
+                        timestamp = last.timestamp
+                        // Use the lastMessage here
+                        print("Last Message: \(String(describing: lastMessage))")
+                    } else {
+                        print("see 9")
+                        // The array is empty
+                        lastMessage = ""
+                        timestamp = ""
                     }
-            
+                    
+                    if let lastMessageText = lastMessage, let lastMessageTimestamp = timestamp {
+                        if let index = self.friends.firstIndex(where: { $0.email == email }) {
+                            self.friends[index].lastMessage = lastMessageText
+                        } else {
+                            let friend = Friend(name: name, lastMessage: lastMessageText, time: lastMessageTimestamp, email: email)
+                            print("see 10: ", self.friends.count)
+                            self.friends.append(friend)
+                        }
+
+                    } else {
+                        // Handle the case where lastMessageText or lastMessageTimestamp is nil
+                        print("No last message available")
+                    }
+                    // Sort the friends array based on timestamp
+                    self.friends.sort { (friend1, friend2) -> Bool in
+                        // Assuming timestamp is a String in the format of a date
+                        return friend1.time > friend2.time
+                    }
+                    self.msgView.tableViewMessages.reloadData()
+                }
             } )
-//        self.helperObj.getMessages(chatID: chatID) { (messages) in
-//            if let messageArray = messages {
-//                for msg in messageArray {
-//                    print("should be here")
-//                    msgArray.append(msg)
-//                }
-//                print("Hello: ")
-//                print(msgArray)
-//                var lastMessage: String?
-//                var timestamp: String?
-//                
-//                if let last = msgArray.last {
-//                    lastMessage = last.text
-//                    timestamp = last.timestamp
-//                    // Use the lastMessage here
-//                    print("Last Message: \(String(describing: lastMessage))")
-//                } else {
-//                    // The array is empty
-//                    lastMessage = ""
-//                    timestamp = ""
-//                }
-//
-//                if let lastMessageText = lastMessage, let lastMessageTimestamp = timestamp {
-//                    let friend = Friend(name: name, lastMessage: lastMessageText, time: lastMessageTimestamp, email: email)
-//                    print("Check: ", lastMessageText)
-//                    self.friends.append(friend)
-//
-//                } else {
-//                    // Handle the case where lastMessageText or lastMessageTimestamp is nil
-//                    print("No last message available")
-//                }
-//                // Sort the friends array based on timestamp
-//                self.friends.sort { (friend1, friend2) -> Bool in
-//                    // Assuming timestamp is a String in the format of a date
-//                    return friend1.time > friend2.time
-//                }
-//                self.msgView.tableViewMessages.reloadData()
-//            }
-//        }
     }
     
     //Change from String to Chat Model
